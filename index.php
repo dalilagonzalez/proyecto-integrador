d5981209485d1150a828e899849a04454df5d805
Jose <jgdwindt@gmail.com>
2019-05-03 20:11:06 -0300

    primer commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+$nombre = "Jose";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans|Roboto" rel="stylesheet">
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <title>pIntegrador</title>
+  </head>
+  <body>
+
+    <h1>"Hola, soy <?= $nombre?>" </h1>
+
+
+  </body>
+</html>
---
+++ login.php
@@ -0,0 +1,31 @@
+<?php
+
+$nombre = "Jose";
+
+?>
+
+<!DOCTYPE html>
+<html lang="sp" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans|Roboto" rel="stylesheet">
+    <link rel="stylesheet" href="css/login.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <title>Login Proyecto Integrador</title>
+  </head>
+  <body>
+    <header>
+      <h1>INCLUIR HEADER DE LOGIN</h1>
+    </header>
+    <section>
+      <ul>
+        <li>NOMBRE</li>
+        <li>PASSWORD</li>
+      </ul>
+      <p>registrarse</p>
+      <p>olvidé mi contraseña</p>
+
+    </section>
+  </body>
+</html>
