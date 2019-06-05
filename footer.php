5227eb4f3be1a45854decb8810729a58eb937b53
ottox11 <49925267+ottox11@users.noreply.github.com>
2019-06-04 21:16:05 -0300

    mejoras

--- footer.php
+++ footer.php
@@ -1,104 +1,131 @@
-<footer class="page-footer font-small indigo">
+<!-- Footer -->
+<footer class="page-footer font-small mdb-color pt-4">
 
+  <!-- Footer Links -->
+  <div class="container text-center text-md-left">
 
-  <div class="container">
+    <!-- Footer links -->
+    <div class="row text-center text-md-left mt-3 pb-3">
 
-
-    <div class="row text-center d-flex justify-content-center pt-5 mb-3">
-
-
-      <div class="col-md-2 mb-3">
-        <h6 class="text-uppercase font-weight-bold">
-          <a href="#!">Superpets</a>
-        </h6>
+      <!-- Grid column -->
+      <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+        <h6 class="text-uppercase mb-4 font-weight-bold">Mundo Mascotas</h6>
+        <p>Consigue todo lo que necesitas para tus mascotas. </p>
       </div>
+      <!-- Grid column -->
 
-      <div class="col-md-2 mb-3">
-        <h6 class="text-uppercase font-weight-bold">
-          <a href="#!">Productos</a>
-        </h6>
-      </div>
+      <hr class="w-100 clearfix d-md-none">
 
-      <div class="col-md-2 mb-3">
-        <h6 class="text-uppercase font-weight-bold">
+      <!-- Grid column -->
+      <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+        <h6 class="text-uppercase mb-4 font-weight-bold">Productos</h6>
+        <p>
+          <a href="#!">Tiendas</a>
+        </p>
+        <p>
+          <a href="#!">Albergues</a>
+        </p>
+        <p>
           <a href="#!">Servicios</a>
-        </h6>
+        </p>
+        <p>
+          <a href="#!">Adopta a una mascota</a>
+        </p>
+      </div>
+      <!-- Grid column -->
+
+      <hr class="w-100 clearfix d-md-none">
+
+      <!-- Grid column -->
+      <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
+        <h6 class="text-uppercase mb-4 font-weight-bold">links de Utilidad</h6>
+        <p>
+          <a href="#!">Tu Cuenta</a>
+        </p>
+        <p>
+          <a href="#!">Registrate</a>
+        </p>
+        <p>
+          <a href="#!">Mascotas Perdidas </a>
+        </p>
+        <p>
+          <a href="#!">Preguntas Frecuentes</a>
+        </p>
       </div>
 
-      <div class="col-md-2 mb-3">
-        <h6 class="text-uppercase font-weight-bold">
-          <a href="#!">Ayuda</a>
-        </h6>
-      </div>
+      <!-- Grid column -->
+      <hr class="w-100 clearfix d-md-none">
 
-      <div class="col-md-2 mb-3">
-        <h6 class="text-uppercase font-weight-bold">
-          <a href="#!">Contacto</a>
-        </h6>
+      <!-- Grid column -->
+      <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
+        <h6 class="text-uppercase mb-4 font-weight-bold">Contacto</h6>
+        <p>
+          <i class="fas fa-home mr-3"></i> Buenos Aires, CABA, AR</p>
+        <p>
+          <i class="fas fa-envelope mr-3"></i> info@mundomascotas.com</p>
+        <p>
+          <i class="fas fa-phone mr-3"></i> 11 11111111</p>
+        <p>
+          <i class="fas fa-print mr-3"></i> 22 22222222</p>
       </div>
-
+      <!-- Grid column -->
 
     </div>
+    <!-- Footer links -->
 
-    <hr class="rgba-white-light" style="margin: 0 15%;">
+    <hr>
 
+    <!-- Grid row -->
+    <div class="row d-flex align-items-center">
 
-    <div class="row d-flex text-center justify-content-center mb-md-0 mb-4">
+      <!-- Grid column -->
+      <div class="col-md-7 col-lg-8">
 
+        <!--Copyright-->
+        <p class="text-center text-md-left">© 2019 Copyright:
+          <strong> mundomascotas.com</strong>
+          </p>
 
-      <div class="col-md-8 col-12 mt-5">
-        <p style="line-height: 1.7rem"> Actualmente disponemos de dos sucursales ubicadas en barrios centrales de Capital Federal. Podes contactarnos online, telefónicamente o bien visitarnos para conocer nuestro local! <br> Te esperamos. </p>
       </div>
+      <!-- Grid column -->
 
+      <!-- Grid column -->
+      <div class="col-md-5 col-lg-4 ml-lg-0">
 
-    </div>
-
-    <hr class="clearfix d-md-none rgba-white-light" style="margin: 10% 15% 5%;">
-
-
-    <div class="row pb-3">
-
-
-      <div class="col-md-12">
-
-        <div class="mb-5 flex-center">
-
-
-          <a class="fb-ic">
-            <i class="fab fa-facebook-f fa-lg white-text mr-4"> </i>
-          </a>
-
-          <a class="tw-ic">
-            <i class="fab fa-twitter fa-lg white-text mr-4"> </i>
-          </a>
-
-          <a class="gplus-ic">
-            <i class="fab fa-google-plus-g fa-lg white-text mr-4"> </i>
-          </a>
-
-          <a class="li-ic">
-            <i class="fab fa-linkedin-in fa-lg white-text mr-4"> </i>
-          </a>
-
-          <a class="ins-ic">
-            <i class="fab fa-instagram fa-lg white-text mr-4"> </i>
-          </a>
-
-          <a class="pin-ic">
-            <i class="fab fa-pinterest fa-lg white-text"> </i>
-          </a>
-
+        <!-- Social buttons -->
+        <div class="text-center text-md-right">
+          <ul class="list-unstyled list-inline">
+            <li class="list-inline-item">
+              <a class="btn-floating btn-sm rgba-white-slight mx-1">
+                <i class="fab fa-facebook-f"></i>
+              </a>
+            </li>
+            <li class="list-inline-item">
+              <a class="btn-floating btn-sm rgba-white-slight mx-1">
+                <i class="fab fa-twitter"></i>
+              </a>
+            </li>
+            <li class="list-inline-item">
+              <a class="btn-floating btn-sm rgba-white-slight mx-1">
+                <i class="fab fa-google-plus-g"></i>
+              </a>
+            </li>
+            <li class="list-inline-item">
+              <a class="btn-floating btn-sm rgba-white-slight mx-1">
+                <i class="fab fa-linkedin-in"></i>
+              </a>
+            </li>
+          </ul>
         </div>
 
       </div>
-
+      <!-- Grid column -->
 
     </div>
-
+    <!-- Grid row -->
 
   </div>
-
-  <div class="footer-copyright text-center py-3">© 2019 Copyright.
-  </div>
+  <!-- Footer Links -->
 
 </footer>
+<!-- Footer -->
